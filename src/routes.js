9ef855752da968875319c84a74b788a8b17ced0d
LauraBeatris <laurabeatriserafim@gmail.com>
2020-03-27 18:57:24 -0300

    feat: add padronization to code and commit messages

--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,38 @@
-const express = require('express')
-const routes = express.Router()
+const express = require('express');
 
-const { recipes } = require('./data')
+const routes = express.Router();
+const data = require('./data');
 
-const RecipesController = require('./app/controllers/RecipesController')
+const RecipesController = require('./app/controllers/RecipesController');
 
 /*  
     == Admin Routes ==
 */
 
 // TODO - Authentication
-routes.get("/admin", (req, res) => res.redirect(301, "/admin/recipes"))
-routes.get("/admin/recipes", RecipesController.index);
-routes.get("/admin/recipes/create", RecipesController.create);
-routes.get("/admin/recipes/:id", RecipesController.show);
-routes.get("/admin/recipes/:id/edit", RecipesController.edit);
+routes.get('/admin', (req, res) => res.redirect(301, '/admin/recipes'));
+routes.get('/admin/recipes', RecipesController.index);
+routes.get('/admin/recipes/create', RecipesController.create);
+routes.get('/admin/recipes/:id', RecipesController.show);
+routes.get('/admin/recipes/:id/edit', RecipesController.edit);
 
-routes.post("/admin/recipes", RecipesController.post);
-routes.put("/admin/recipes/:id", RecipesController.put); 
-routes.delete("/admin/recipes/:id", RecipesController.delete); 
+routes.post('/admin/recipes', RecipesController.post);
+routes.put('/admin/recipes/:id', RecipesController.put);
+routes.delete('/admin/recipes/:id', RecipesController.delete);
 
 /*  
     == Public Routes ==
 */
-routes.get('/', (req, res) => res.render('public/index', { recipes }))
-routes.get('/recipes', (req, res) => res.render('public/recipes/index', { recipes }))
-routes.get('/recipes/:id', (req, res) => { 
-    const recipe = recipes[req.params.id]
-    res.render('public/recipes/show', { recipe })
-})
-routes.get('/about', (_, res) => res.render('public/about'))
+routes.get('/', (req, res) =>
+    res.render('public/index', { recipes: data.recipes })
+);
+routes.get('/recipes', (req, res) =>
+    res.render('public/recipes/index', { recipes: data.recipes })
+);
+routes.get('/recipes/:id', (req, res) => {
+    const recipe = data.recipes[req.params.id];
+    res.render('public/recipes/show', { recipe });
+});
+routes.get('/about', (_, res) => res.render('public/about'));
 
-
-module.exports = routes
+module.exports = routes;
