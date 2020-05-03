7ce139179d33906b0960a14dce7a611eb70a92c3
Laura <laurabeatriserafim@gmail.com>
2020-05-03 15:57:31 -0300

    fix: remove not null constraint from password column of user table

--- database.sql
+++ database.sql
@@ -10,7 +10,7 @@ CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
 	name TEXT NOT NULL,
 	email TEXT UNIQUE NOT NULL,
-	password TEXT NOT NULL,
+	password TEXT,
 	reset_token TEXT,
 	reset_token_expires TEXT,
 	is_admin BOOLEAN DEFAULT false,
