8b60d0b799063d42c0cab97f9b74fd66f8a632cf
Laura <laurabeatriserafim@gmail.com>
2020-05-02 22:44:04 -0300

    fix: change user property name of database config

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 module.exports = new Pool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
+    user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
 });
