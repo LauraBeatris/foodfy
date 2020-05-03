514663ac641ee2cc62d8a836abfd48da06426852
Laura <laurabeatriserafim@gmail.com>
2020-05-03 16:04:20 -0300

    fix: add validator to route and fix elements attributes

--- src/routes/admin/user.routes.js
+++ src/routes/admin/user.routes.js
@@ -3,9 +3,10 @@ const express = require('express');
 const routes = express.Router();
 
 const UserController = require('../../app/controllers/admin/UserController');
+const UserValidator = require('../../app/validators/UserValidator');
 
 routes.get('/users', UserController.list);
-routes.post('/users', UserController.post);
+routes.post('/users', UserValidator.post, UserController.post);
 routes.get('/users/create', UserController.create);
 routes.get('/users/:id/edit', UserController.edit);
 routes.put('/users/:id', UserController.put);
